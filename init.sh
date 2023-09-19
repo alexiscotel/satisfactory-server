dce34f34e46366bc2d9d4ce183a421965fec48af
Alexis Cotel <contact@alexiscotel.fr>
2023-09-19 22:10:32 +0200

    update doc

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 #!bin/bash
 
-dpkg --add-architecture i386; apt update; 
-apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 distro-info firewalld nodejs
+dpkg --add-architecture i386; apt update; apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 distro-info firewalld nodejs
 
 adduser sfserver
 su - sfserver
